95dded1f90c4752098249e0f14d8597ff4a3acc6
montes8 <emontesinos812@gmail.com>
2018-12-15 11:30:00 -0500

    creando activityLogin

--- app/src/main/java/com/example/tayler_gabbi/mvp_kotlin/LoginActivity.kt
+++ app/src/main/java/com/example/tayler_gabbi/mvp_kotlin/LoginActivity.kt
@@ -3,14 +3,10 @@ package com.example.tayler_gabbi.mvp_kotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
-
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_login)
     }
 }
