62e75d8e5adb3ce36355ef12870fa527f8d5b22a
montes8 <emontesinos812@gmail.com>
2018-12-17 20:12:12 -0500

    creando base datos

---
+++ app/src/main/java/com/example/tayler_gabbi/mvp_kotlin/database/ConexionSQLiteHelper.kt
@@ -0,0 +1,84 @@
+package com.example.tayler_gabbi.mvp_kotlin.database
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.SQLException
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.database.sqlite.SQLiteQueryBuilder
+import android.util.Log
+import android.widget.Toast
+import java.util.ArrayList
+
+class ConexionSQLiteHelper{
+
+    val dbNombre="db_mvp"
+    val dbVersion=1
+
+    val dbTablaUser="Usuarios"
+    val columnaUserID="ID"
+    val columnaUsuario="Usuario"
+    val columnaContraseña="password"
+    val sqlCrearTablaUser ="CREATE TABLE IF NOT EXISTS " + dbTablaUser +" ("+ columnaUserID +" INTEGER PRIMARY KEY AUTOINCREMENT,"+
+            columnaUsuario +" TEXT NOT NULL,"+
+            columnaContraseña +" TEXT NOT NULL)"
+    ////////////////////////////////////////////////////////////////////////
+
+
+    var sqlDB:SQLiteDatabase?=null
+    constructor(contexto: Context){
+        val db=DBHerlperFunciones(contexto)
+        sqlDB=db.writableDatabase
+    }
+
+
+    inner class DBHerlperFunciones(contexto: Context):SQLiteOpenHelper(contexto,dbNombre,null,dbVersion){
+
+        var contexto:Context=contexto
+
+        override fun onCreate(db: SQLiteDatabase?) {
+
+            db!!.execSQL(sqlCrearTablaUser)
+            Toast.makeText(this.contexto,"la base de datos administrar Dinero a sido creada",Toast.LENGTH_LONG).show()
+        }
+
+        override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+            db!!.execSQL("Drop table IF EXISTS"+dbTablaUser)
+        }
+    }
+
+
+
+    fun insertarUser(values: ContentValues):Long{
+
+        val ID =sqlDB!!.insert(dbTablaUser,"",values)
+        return ID
+    }
+    fun queryUser(projection:Array<String>,selection:String,selectionArgs:Array<String>,orderBy:String): Cursor {
+
+        val consulta= SQLiteQueryBuilder()
+        consulta.tables=dbTablaUser
+        val cursor=consulta.query(sqlDB,projection,selection,selectionArgs,null,null,orderBy)
+        return cursor
+    }
+    fun borrarUser(selection: String,selectionArgs: Array<String>):Int{
+        val contador=sqlDB!!.delete(dbTablaUser,selection,selectionArgs)
+        return contador
+    }
+    fun actualizarUser(values: ContentValues, selection: String, selectionArgs: Array<String>):Int{
+        val contador=sqlDB!!.update(dbTablaUser,values,selection,selectionArgs)
+        return contador
+    }
+    fun userLogin(usuario:String,contraseña:String):Boolean{
+        val query="select * from Usuarios where Usuario = '$usuario' and password = '$contraseña'"
+        val cursor=sqlDB!!.rawQuery(query,null)
+        if (cursor.count<=0){
+            cursor.close()
+            return false
+        }
+        cursor.close()
+        return true
+
+    }
+}
---
+++ app/src/main/java/com/example/tayler_gabbi/mvp_kotlin/database/Usuario.kt
@@ -0,0 +1,7 @@
+package com.example.tayler_gabbi.mvp_kotlin.database
+
+data class Usuario (
+        var usuId:Int?=null,
+        var usuario:String?,
+        var password:String
+)
