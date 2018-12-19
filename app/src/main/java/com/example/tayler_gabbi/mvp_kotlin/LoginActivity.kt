f58e0885bba67f9414497f19911f933bbb8a86e1
montes8 <emontesinos812@gmail.com>
2018-12-18 20:11:10 -0500

    agregando codigo en el activity

--- app/src/main/java/com/example/tayler_gabbi/mvp_kotlin/LoginActivity.kt
+++ app/src/main/java/com/example/tayler_gabbi/mvp_kotlin/LoginActivity.kt
@@ -4,19 +4,28 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.tayler_gabbi.mvp_kotlin.database.ConexionSQLiteHelper
 import com.example.tayler_gabbi.mvp_kotlin.model.PresenterImpl
 import com.example.tayler_gabbi.mvp_kotlin.presenter.LoginPresenter
 import com.example.tayler_gabbi.mvp_kotlin.view.LoginView
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity(),LoginView {
 
-    var loginPresenter : LoginPresenter? = null
+    private var loginPresenter : LoginPresenter? = null
+
+    var conn: ConexionSQLiteHelper? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         loginPresenter = PresenterImpl(this)
+        conn = ConexionSQLiteHelper(this)
+
+        button_ingresar.setOnClickListener {
+            loginPresenter!!.perfomLogin(edit_usuario.text.toString(),edit_pasword.text.toString(), conn!!)
+        }
 
 
     }
