dfa5426b147d50bee8267c2c1ac976d30db43bda
montes8 <emontesinos812@gmail.com>
2018-12-19 08:05:17 -0500

    implementando registroView

--- app/src/main/java/com/example/tayler_gabbi/mvp_kotlin/model/RegistroPresenterImpl.kt
+++ app/src/main/java/com/example/tayler_gabbi/mvp_kotlin/model/RegistroPresenterImpl.kt
@@ -1,6 +1,45 @@
 package com.example.tayler_gabbi.mvp_kotlin.model
 
-class RegistroPresenterImpl {
+import com.example.tayler_gabbi.mvp_kotlin.database.Usuario
+import android.text.TextUtils
+import com.example.tayler_gabbi.mvp_kotlin.database.ConexionSQLiteHelper
+import com.example.tayler_gabbi.mvp_kotlin.presenter.RegistroPresenter
+import com.example.tayler_gabbi.mvp_kotlin.view.RegistroView
 
 
+class RegistroPresenterImpl(private val registrarView: RegistroView) : RegistroPresenter{
+
+  override fun
+     registrarUsuario(name: String, userName: String, password: String, conn: ConexionSQLiteHelper) {
+
+        if (TextUtils.isEmpty(name)) {
+
+            registrarView.nameError()
+
+        } else if (TextUtils.isEmpty(userName)) {
+
+            registrarView.usuarioError()
+        } else if (TextUtils.isEmpty(password)) {
+
+            registrarView.passwordError()
+
+        } else {
+
+            val usuarioi = Usuario()
+            usuarioi.usuario = userName
+            usuarioi.password = password
+            val idResultante = conn.insertarUser(usuarioi)
+
+            if (idResultante != null && idResultante > 0) {
+
+                registrarView.registrarSuccess()
+
+            } else {
+
+                registrarView.registerError()
+            }
+
+
+        }
+    }
 }
